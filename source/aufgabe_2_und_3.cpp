39823e48cddc286a1fb680d0aae83e769e9d14b8
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-07-01 17:59:54 +0200

    now 100% more random

--- source/aufgabe_2_und_3.cpp
+++ source/aufgabe_2_und_3.cpp
@@ -1,14 +1,20 @@
 #include <vector>
-#include <iostream>
 #include <set>
+#include <map>
+
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+
+#include <iostream>
 
 int main(int argc, char* argv[])
 {
   //4.2
 
   std::vector<unsigned int> vec(100);
-  std::transform (vec.begin(), vec.end(), vec.begin(), [] (unsigned int el) -> auto { return rand() % 101;});
+  std::srand(std::time(0));
+  std::transform (vec.begin(), vec.end(), vec.begin(), [] (unsigned int el) -> auto { return rand()% 101;});
   std::set<unsigned int> st{};
 
   //4.2.1
@@ -24,7 +30,7 @@ int main(int argc, char* argv[])
   std::cout << "4.2.2 - All the elements not present in the vector:\n";
   auto itr = st.begin();
   int lnbrk = 10;
-  for (int i = 0; i < 101; ++i) {
+  for (unsigned int i = 0; i < 101; ++i) {
     if (*itr == i) {
       ++itr;
     } 
@@ -33,10 +39,18 @@ int main(int argc, char* argv[])
         std::cout << "\n\t";
         lnbrk = 0;
       }
-      (*st.rbegin()-1 == i) ? std::cout << i << "\n\n" : std::cout << i << ", ";
+      (*itr == *st.rbegin()) ? std::cout << i << "\n\n" : std::cout << i << ", "; //st.rbegin() behaving strange 
       lnbrk++;
     }
-  }  
+  }
+  
+  //4.3
+
+  std::cout << "\n4.3 - Elements and their number of occurrences\n\n";
+  std::map<unsigned int, unsigned int> mp{};
+  for (auto i : vec) {
+    //idk duuud
+  }
  /*
  for (auto i : vec) {
    std::cout << i << "\n";
