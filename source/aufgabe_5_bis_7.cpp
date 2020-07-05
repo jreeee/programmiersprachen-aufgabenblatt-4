db76b86252f01454262d30cffa684ec1b702eab6
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-07-05 07:58:12 +0200

    4.7 added another functor example

--- source/aufgabe_5_bis_7.cpp
+++ source/aufgabe_5_bis_7.cpp
@@ -50,18 +50,22 @@ class comp {
 
 TEST_CASE("testing if the circle list is sorted using functor - 4.7", "[srtd_circ_7]")
 {
-  
-
-  //Initializing a list containing circles and filling it with circles with random radius
-  std::list<Circle> sorted_circles;
+  //Initializing a list and filling it with circles with random radius, then making a copy of that list
+  std::list<Circle> sorted_circles_1;
   int sc_size = 20;
   for (int i = 0; i < sc_size; ++i) {
-    sorted_circles.push_back(Circle(float(rand() % 501)));
+    sorted_circles_1.push_back(Circle(float(rand() % 501)));
   }
-  //sorting the list and checking if it was sorted correctly using the lambda to compare the radii
-  auto sorting_functor = (sorted_circles.begin(), sorted_circles.end(), comp<Circle>{});
-  sorted_circles.sort(sorting_functor);
-  REQUIRE(std::is_sorted(sorted_circles.begin(), sorted_circles.end()));
+  std::list<Circle> sorted_circles_2 {sorted_circles_1};
+
+  //sorting the lists using the functor defined earlier for one list 
+  //and for the other one the functor std::less, then checking if both lists are sorted
+  auto sorting_functor_1 = (sorted_circles_1.begin(), sorted_circles_1.end(), comp<Circle>{});
+  auto sorting_functor_2 = (sorted_circles_2.begin(), sorted_circles_2.end(), std::less<Circle>());
+  sorted_circles_1.sort(sorting_functor_1);
+  sorted_circles_2.sort(sorting_functor_2);
+  REQUIRE(std::is_sorted(sorted_circles_1.begin(), sorted_circles_1.end()));
+  REQUIRE(std::is_sorted(sorted_circles_2.begin(), sorted_circles_2.end()));
 }
 
 
