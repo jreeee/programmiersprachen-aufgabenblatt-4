989962ca9df9e2d1a470a88e7068f3abe0575aba
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-07-05 21:49:18 +0200

    something's still off

--- source/aufgabe_12.cpp
+++ source/aufgabe_12.cpp
@@ -3,21 +3,23 @@
 #include <cmath>      
 #include <algorithm>  //std::remove_if, std::all_of, std::generate
 #include <vector>     //std::vector<> 
-#include <cstdlib>
 #include <functional>
 
 template<class T, class UnaryPredicate> 
-//T filter(T const& s, const std::function<bool(const element&)>predicate)
 T filter(T s, UnaryPredicate p)
 {
   T nw;
-  //std::copy (s.begin(), s.end(), nw.end());
-  //return (std::remove_if(nw.begin(), nw.end(), p));
-  return (std::copy_if (s.begin(), s.end(), nw.end(), p));
+  using std::begin;
+  using std::end;
+  std::copy (begin(s), end(s), std::back_inserter(nw));
+  //return (std::remove_if(begin(nw), end(nw), p));
+  //return std::copy_if (begin(s), end(s), std::backinserter(nw), p);
+  return nw;
 }
 
 TEST_CASE()
 {
+  //bool is_even = [] (int n) { return n % 2 == 0; };
   std::function<bool(int)>is_even = [] (int n) { return n % 2 == 0; };
   std::vector<int> v{1, 2, 3, 4, 5, 6};
   std::vector<int> all_even = filter(v, is_even);
