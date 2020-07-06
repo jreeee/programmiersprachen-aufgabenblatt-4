e1c83ca78c524812a58ac84b6b39dfa4759f0714
Jeremias Kilian <jeremias.michael.kilian@uni-weimar.de>
2020-07-06 09:39:44 +0200

    a_14 works :) missing comments

--- source/aufgabe_14.cpp
+++ source/aufgabe_14.cpp
@@ -5,6 +5,10 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <iterator>   //std::ostream_iterator<>
+#include <functional>
+#include <thread>
+#include <chrono>
 
 #include "circle.hpp"
 #include "rectangle.hpp"
@@ -13,11 +17,11 @@
 
 #include <cmath>
 
+
 int main(int argc, char* argv[])
 {
     Window win{std::make_pair(800,800)};
-    std::multiset<std::string, Circle> circles;
-    auto str_crc_pair = std::make_pair<std::string, Circle> ;
+    
     std::vector<Circle> tmp {{{400.0f, 400.0f}, 380.0f, {0.1f, 0.3f, 0.6f}, "hans"},
                               {{100.0f, 100.0f}, 100.0f, {0.2f, 0.4f, 0.9f}, "fritz"},
                               {{700.0f, 700.0f}, 100.0f, {0.2f, 0.4f, 0.9f}, "dieter"},
@@ -33,27 +37,47 @@ int main(int argc, char* argv[])
                               {{0.0f, 0.0f}, 75.0f, {0.4f, 0.6f, 1.0f}, "alice"},
                               {{0.0f, 800.0f}, 75.0f, {0.4f, 0.6f, 1.0f}, "dora"},
                               {{800.0f, 0.0f}, 75.0f, {0.4f, 0.6f, 1.0f}, "luise"},
-                              {{800.0f, 800.0f}, 75.0f, {0.4f, 0.6f, 1.0f}, ""}};
+                              {{800.0f, 800.0f}, 75.0f, {0.4f, 0.6f, 1.0f}, "test"}};
 
-    std::for_each(tmp.begin(), tmp.end(), [] 
-                  (Circle const& c, std::multiset<Circle> & circles, auto & str_crc_pair) 
-                  {circles.emplace(str_crc_pair = (c.name(), c));});
+    std::multiset<Circle> circles;
 
-    float passed = win.get_time();
+    for (auto i : tmp) {
+        circles.emplace(i);
+    }
 
+    auto comp = [] (std::string const& s, Circle c) {return s == c.name(); };
 
     while (!win.should_close()) {
         if (win.get_key(GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         win.close();
         }
-        passed = win.get_time();
+        std::cout << "\nthe following circles can be highlighted:\n\n";
+        for (auto i : circles) {
+            std::cout << i.name() << " ";
+        }
+        std::cout << "\n\nplease enter the name of the circle you want to highight\n\n";
+        std::string str; 
+        std::cin >> str;
+        bool cont_circ;
+        bool found = false;
+        for (auto i : circles) {
+            cont_circ = comp(str, i);
+            cont_circ ? i.draw(win, true, 2.0f) : i.draw(win, false, 2.0f); 
+            if (!found && cont_circ) found = true;
+        }
 
-        /*hr.line(win, 360.0f, passed, 4.0f);
-        min.line(win, 60.0f, passed, 3.0f);
-        sec.line(win, 1.0f, passed, 2.0f);
-        outer.draw(win, false,  5.0f);
-       inner.draw(win, false, 5.0f);*/ 
         win.update();
+        
+        if (found) {
+            std::cout << "\n-> hightlighting circle " << str << " for 10 seconds\n";
+            std::this_thread::sleep_for(std::chrono::seconds(10));
+        } else {
+            std::cout << "\n-> the specified circle could not be found\n";
+        }
+
+        
+
+        
     }
     return 0;
 }
